ada2063d4bc259055372b47b66240906b10e9d5d
Emily <efg12103@gmail.com>
2020-03-26 10:05:56 -0400

    adding to main

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 
 public class Main {
+    private Scanner in;
+
 
     public static void main (String[] args) {
-        
+        startup();
+    }
+
+    public Main() {
+        this.in = new Scanner(System.in);
+    }
+
+    public void startup() {
+
     }
 }
