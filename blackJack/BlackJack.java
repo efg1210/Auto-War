01102ab189c32a6999ea0fc9995b9261b01e9f0b
Emily <efg12103@gmail.com>
2020-03-30 09:58:59 -0400

    starting the basics

--- blackJack/BlackJack.java
+++ blackJack/BlackJack.java
@@ -1,7 +1,22 @@
 
 public class BlackJack {
     
-    public BlackJack(Scanner in) {
-        
+    Scanner in;
+    int balance;
+
+    public BlackJack(Scanner in, int balance) {
+        this.in = in;
+        this.balance = balance;
+    }
+
+    public start() {
+        boolean continue = true;
+        while (balance > 0 && continue) {
+            int bet;
+            System.out.println("Your balance: " + balance + " G Dollars");
+            System.out.print("Place your bet: ");
+            bet = in.nextInt();
+            in.nextLine();
+        }
     }
 }
