90760705dec84ab3c8d44bdf39efa46984ccd17d
Emily <efg12103@gmail.com>
2020-04-07 10:03:00 -0400

    made to String for card

--- Card.java
+++ Card.java
@@ -36,4 +36,49 @@ public class Card implements Comparable<Card> {
         }
         return 0;
     }
+
+    @Override
+    public String toString() {
+        String message = "";
+        switch (getRank()) {
+            case "2":
+                message += "Two of"; break;
+            case "3":
+                message += "Three of"; break;
+            case "4":
+                message += "Four of"; break;
+            case "5":
+                message += "Five of"; break;
+            case "6":
+                message += "Six of"; break;
+            case "7":
+                message += "Seven of"; break;
+            case "8":
+                message += "Eight of"; break;
+            case "9":
+                message += "Nine of"; break;
+            case "T":
+                message += "Ten of"; break;
+            case "J":
+                message += "Jack of"; break;
+            case "Q":
+                message += "Queen of"; break;
+            case "K":
+                message += "King of"; break;
+            case "A":
+                message += "Ace of"; break;
+            }
+
+        if (getSuit().equals("C")) {
+            message += " Clubs";
+        } else if (getSuit().equals("D")) {
+            message += " Diamonds";
+        } else if (getSuit().equals("H")) {
+            message += " Hearts";
+        } else if (getSuit().equals("S")) {
+            message += " Spades";
+        }
+        
+        return message;
+    }
 }
