1459052e57897dca44e484fb6dff596db94a25e8
Emily <efg12103@gmail.com>
2020-04-16 19:17:05 -0400

    there is totally a better way to do this but i don't know it so

---
+++ src/Poker.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Poker {
+    Scanner in;
+    int balance;
+    
+    public Poker(Scanner in, int balance) {
+    	this.in = in;
+    	this.balance = balance;
+    }
+    
+    public static void start() {
+    	Scanner in = new Scanner(System.in);
+    	System.out.print("Poker:\nPlace a bet:");
+    	int betInt = in.nextInt();
+    	if(betInt < 1) {
+    		System.out.println("Minimum bet is 1 chip");
+    	} else if (betInt > 10) {
+    		System.out.println("Maximum bet is 10 chips");
+    	}
+    }
+}
