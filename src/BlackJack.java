99ef6ffec876d132e4e352ae06185b5e60cc2e8c
Emily <efg12103@gmail.com>
2020-04-17 19:55:16 -0400

    yay progress

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -29,14 +29,14 @@ public class BlackJack {
     }
 
     public void start() {
-        System.out.println("Welcome to Black Jack, " + getName() + "!");
+        System.out.println("\nWelcome to Blackjack, " + getName() + "!");
         deck.populate();
         deck.shuffle();
         boolean continuePlaying = true;
         while (getBalance() > 0 && continuePlaying) {
             int bet = 0;
             while (bet <= 0 || bet > 10 || bet > balance) {
-                System.out.println("Your balance: " + getBalance() + " G Dollars");
+                System.out.println("\nYour balance: " + getBalance() + " G Dollars");
                 System.out.print("Place your bet: ");
                 bet = in.nextInt();
                 in.nextLine();
@@ -51,21 +51,22 @@ public class BlackJack {
             dealerHand.add((Card) deck.remove((int) Math.random() * deck.size()));
             dealerHand.add((Card) deck.remove((int) Math.random() * deck.size()));
 
-            int userPts = 0;
-            int dealerPts = 0;
+            int userPts = calculatePts(userHand);
+            int dealerPts = calculatePts(dealerHand);
 
             update(userHand, dealerHand);
 
             boolean stay = false;
-            while (!stay) {
-                System.out.println("\nOptions:\n1. Hit\n2. Stay\n3. Sort cards");
+            if (userPts == 21) {
+                stay = true;
+            }
+
+            while (!stay && userPts < 21) {
+                System.out.println("\nOptions:\n1. Hit\n2. Stay\n3. Sort cards\n");
                 String answer = in.nextLine();
                 if (answer.equals("1") || (answer.toLowerCase()).equals("hit")) {
                     userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
                     userPts = calculatePts(userHand);
-                    if (userPts >= 21) {
-                        stay = true;
-                    }
                 } else if (answer.equals("2") || (answer.toLowerCase()).equals("stay")) {
                     stay = true;
                 } else if (answer.equals("3") || (answer.toLowerCase()).equals("sort") || (answer.toLowerCase()).equals("sort cards")) {
@@ -74,7 +75,52 @@ public class BlackJack {
                 update(userHand, dealerHand);
             }
 
-            continuePlaying = response("Do you want to continue playing (y/n): ");
+            int target = 17;
+            boolean containsAce = false;
+
+            for (Card card : dealerHand) {
+                if (card.getRank() == 1) {
+                    containsAce = true;
+                    target = 18;
+                }
+            }
+
+            while (dealerPts < target) {
+                dealerHand.add((Card) deck.remove((int) Math.random() * deck.size()));
+                for (Card card : dealerHand) {
+                    if (card.getRank() == 1) {
+                        containsAce = true;
+                        target = 18;
+                    }
+                }
+                dealerPts = calculatePts(dealerHand);
+            }
+
+            System.out.println("\nYour points: " + userPts);
+            System.out.println("Dealer points: " + dealerPts + "\n");
+
+            if (userHand.size() == 2 && userPts == 21) {
+                System.out.println("Blackjack! You win an additional " + (bet * 3 / 2) + " G Dollars! Congrats, " + getName() + "!");
+                setBalance(Math.round(bet * 3 / 2) + getBalance());
+            } else if (userPts > 21) {
+                System.out.println("Darn, you lose! You lost " + bet + " G Dollars. Better luck next time.");
+                setBalance(getBalance() - bet);
+            } else if (dealerPts > 21) {
+                System.out.println("You win an additional " + bet + " G Dollars! Congrats, " + getName() + "!");
+                setBalance(bet + getBalance());
+            } else if ((21 - userPts) < (21 - dealerPts)) {
+                System.out.println("You win an additional " + bet + " G Dollars! Congrats, " + getName() + "!");
+                setBalance(bet + getBalance());
+            } else if ((21 - userPts) == (21 - dealerPts)) {
+                System.out.println("Push! You don't win any additional money, but you do keep your bet. Not bad!");
+            } else {
+                System.out.println("Darn, you lose! You lost " + bet + " G Dollars. Better luck next time.");
+                setBalance(getBalance() - bet);
+            }
+
+            String message = (getBalance() == 1) ? "You now have 1 G Dollar." : ("You now have " + getBalance() + " G Dollars.");
+            System.out.println(message);
+            continuePlaying = response("\nDo you want to continue playing (y/n): ");
         }
     }
 
