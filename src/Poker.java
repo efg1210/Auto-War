175520ba9e87f1f2ab550dd744f278df97de67ff
nicolegiron1 <ngiron@ucvts.org>
2020-04-18 14:53:41 -0400

    working on poker

--- src/Poker.java
+++ src/Poker.java
@@ -31,21 +31,55 @@ public class Poker {
         deck.populate();
         deck.shuffle();
         boolean continuePlaying = true;
-        while (getBalance() > 0 && continuePlaying) {
+//        while (getBalance() > 0 && continuePlaying) {
         	int bet = 0;
-        	while (bet <= 0 || bet >= 10 || bet > balance) {
-        		System.out.println(bet);
+            while (bet <= 0 || bet > 10 || bet > balance) {
                 System.out.println("\nYour balance: " + getBalance() + " G Dollars");
                 System.out.print("Place your bet: ");
                 bet = in.nextInt();
                 in.nextLine();
             }
+            balance -= bet;
+            
         	Deck userHand = new Deck();
         	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
         	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
         	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
-        	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
-        }
+        	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
+        	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
+        	
+//        	int userPts = calculatePts(userHand);
+        	
+        	update(userHand);
+        	
+        	int yn = 0;
+        	int secondBet = 0;
+        	System.out.println("\nPlace another bet?\n1. Yes\n2. No");
+        	yn = in.nextInt();
+        	System.out.println(yn);
+        	if(yn == 1) {
+        		while (secondBet <= 0 || secondBet > 10 || secondBet > balance) {
+                    System.out.println("\nYour balance: " + getBalance() + " G Dollars");
+                    System.out.print("Place your bet: ");
+                    secondBet = in.nextInt();
+                    in.nextLine();
+                }
+        		balance -= secondBet;
+        		bet += secondBet;
+        	}
+        	System.out.println("bet: " + bet);
+        	System.out.println("second bet: " + secondBet);
+        	System.out.println("balance: " + balance);
+        	
+//        }
     	
+    }
+    
+    private void update(Deck userHand) {
+        System.out.println("\nYour hand: ");
+            for (Card card: userHand) {
+                System.out.println(card);
+            }
+//            System.out.println("Your points: " + calculatePts(userHand));
     }
 }
