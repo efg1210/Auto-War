9b9ffa43bb1817bc8ecdccb287b535c52055dea5
nicolegiron1 <ngiron@ucvts.org>
2020-04-18 18:26:48 -0400

    all selections except payout

--- src/Poker.java
+++ src/Poker.java
@@ -50,42 +50,63 @@ public class Poker {
         	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
         	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
         	
-//        	int userPts = calculatePts(userHand);
-        	
+        	System.out.println("\nYour hand: ");
         	update(userHand);
         	
         	int yn = 0;
         	int secondBet = 0;
-        	System.out.println("\nPlace another bet?\n1. Yes\n2. No");
-        	yn = in.nextInt();
-        	if(yn == 1) {
-        		while (secondBet <= 0 || secondBet > 10 || secondBet > balance) {
-                    System.out.println("\nYour balance: " + getBalance() + " G Dollars");
-                    System.out.print("Place your bet: ");
-                    secondBet = in.nextInt();
-                    in.nextLine();
-                }
-        		balance -= secondBet;
-        		bet += secondBet;
+        	boolean traded = false;
+        	while(true) {
+        		System.out.println("\nMake a selection:\n1. Place another bet\n2. Trade in\n3. Sort Cards\n4. Payout");
+            	yn = in.nextInt();
+            	if(yn == 1) {
+            		if(secondBet != 0) {
+                		System.out.println("\nSecond bet already made. Choose another option.");
+            		}		
+            		while (secondBet <= 0 || secondBet > 10 || secondBet > balance) {
+                      System.out.println("\nYour balance: " + getBalance() + " G Dollars");
+                      System.out.print("Place your bet: ");
+                      secondBet = in.nextInt();
+                      in.nextLine();
+            		}
+            		balance -= secondBet;
+            		bet += secondBet;
+            	} else if (yn == 2) {
+            		if(!traded) {
+            			int num = 1;
+                		int index = 0;
+                		System.out.println("\nSelect a card to trade:");
+                		update(userHand);
+                		index = in.nextInt();
+                		userHand.remove(index-1);
+                    	userHand.add((Card) deck.remove((int) Math.random() * deck.size()));
+                    	System.out.println("\nNew Hand:");
+                		update(userHand);
+                		traded = true;
+            		} else {
+            			System.out.println("\nOnly one trade is allowed.");
+            		}
+            	} else if (yn == 3) {
+            		Collections.sort(userHand);
+            		System.out.println("\nYour hand sorted: ");
+            		update(userHand);
+            	} else if (yn == 4) {
+            		break;
+            	}
         	}
-        	Collections.sort(userHand);
-        	System.out.println(userHand);
+       
         	
-        	int yn2 = 0;
-        	System.out.println("\nTrade in?\n1. Yes\n2. No");
-        	if(yn2 == 1) {
-        		
-        	}
         	
 //        }
     	
     }
     
     private void update(Deck userHand) {
-        System.out.println("\nYour hand: ");
-            for (Card card: userHand) {
-                System.out.println(card);
-            }
-//            System.out.println("Your points: " + calculatePts(userHand));
+    	int num = 1;
+        for (Card card: userHand) {
+            System.out.println(num + ". " + card);
+            num++;
+        }
     }
+
 }
