55cdd4dd9bf8e052dc8921153be23cf0235e2ecb
Emily <efg12103@gmail.com>
2021-03-04 13:44:12 -0500

    starting interface

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,25 @@ import java.util.*;
 
 public class Main {
     private Scanner in;
+    private Player user;
+    private Player computer;
     public static void main (String[] args) {
         Main main = new Main();
         main.startup();
     }
 
     public Main() {
-        this.in = new Scanner(System.in);
+        in = new Scanner(System.in);
     }
 
     public void startup() {
+        System.out.print("Your name: ");
+        this.user = new Player(in.nextLine());
+        this.computer = new Player("Computer");
+
+        System.out.print("Hello " + user.getName() + "! ");
+        System.out.print("You will be playing against " + computer.getName());
+        System.out.println(" today! Let's play!");
 
         this.in.close();
     }
